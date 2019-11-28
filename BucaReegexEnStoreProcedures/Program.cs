06e1232a722f2deed04d8534e8613bc170d37c90
AvMexico2019 <56038497+AvMexico2019@users.noreply.github.com>
2019-11-28 17:59:33 -0600

    .

--- BucaReegexEnStoreProcedures/Program.cs
+++ BucaReegexEnStoreProcedures/Program.cs
@@ -9,27 +9,29 @@ namespace BucaReegexEnStoreProcedures
         static void Main(string[] args)
         {
             ArrendamientoInmuebleEntities ctx = new ArrendamientoInmuebleEntities();
-            SQL_Formatter.Formatter fmtr = new SQL_Formatter.Formatter();
-
-            var results = ctx.Database.SqlQuery<INFORMATION_SCHEMA_Result>("SELECT ROUTINE_NAME, ROUTINE_DEFINITION FROM INFORMATION_SCHEMA.ROUTINES");
-            foreach(var reg in results)
+            using (System.IO.StreamWriter file =
+            new System.IO.StreamWriter(@"C:\Users\desa21\source\repos\BucaReegexEnStoreProcedures\BucaReegexEnStoreProcedures\Resultados\SPs.txt"))
             {
-                if (U.IsExcelNull(reg.ROUTINE_DEFINITION))
+                var results = ctx.Database.SqlQuery<INFORMATION_SCHEMA_Result>("SELECT ROUTINE_NAME, ROUTINE_DEFINITION FROM INFORMATION_SCHEMA.ROUTINES");
+                foreach (var reg in results)
                 {
-                    Console.WriteLine(reg.ROUTINE_NAME + " ......< definición vacia");
-                }
-                else
-                {
-                    if (UserDefinedFunctions.fncRegexFind("Folio", reg.ROUTINE_DEFINITION.Equals(null) ? " " : reg.ROUTINE_DEFINITION))
+                    if (U.IsExcelNull(reg.ROUTINE_DEFINITION))
                     {
-                        Console.WriteLine("NAME >> " + reg.ROUTINE_NAME + " <<");
-                        Console.WriteLine(reg.ROUTINE_DEFINITION);
-                        Console.WriteLine(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
-                        Console.WriteLine(reg.ROUTINE_DEFINITION);
-                        Console.WriteLine("////////////////////////////==================================");
+                        file.WriteLine(reg.ROUTINE_NAME + " ......< definición vacia");
                     }
-                }
-                   
+                    else
+                    {
+                        if (UserDefinedFunctions.fncRegexFind("Folio", reg.ROUTINE_DEFINITION.Equals(null) ? " " : reg.ROUTINE_DEFINITION))
+                        {
+                            file.WriteLine("NAME >> " + reg.ROUTINE_NAME + " <<");
+                            file.WriteLine(reg.ROUTINE_DEFINITION);
+                            file.WriteLine(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+                            file.WriteLine(reg.ROUTINE_DEFINITION);
+                            file.WriteLine("////////////////////////////==================================");
+                        }
+                    }
+
+                }        
             }
             Console.WriteLine("Fin");
             Console.ReadKey();
