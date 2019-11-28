c2aef74b1449bea49c75eb611f4252a4ba5ca97e
AvMexico2019 <56038497+AvMexico2019@users.noreply.github.com>
2019-11-28 17:53:09 -0600

    .

--- BucaReegexEnStoreProcedures/Program.cs
+++ BucaReegexEnStoreProcedures/Program.cs
@@ -9,7 +9,7 @@ namespace BucaReegexEnStoreProcedures
         static void Main(string[] args)
         {
             ArrendamientoInmuebleEntities ctx = new ArrendamientoInmuebleEntities();
-            SQL_Formatter.Formatter formatter = new SQL_Formatter.Formatter();
+            SQL_Formatter.Formatter fmtr = new SQL_Formatter.Formatter();
 
             var results = ctx.Database.SqlQuery<INFORMATION_SCHEMA_Result>("SELECT ROUTINE_NAME, ROUTINE_DEFINITION FROM INFORMATION_SCHEMA.ROUTINES");
             foreach(var reg in results)
@@ -22,10 +22,10 @@ namespace BucaReegexEnStoreProcedures
                 {
                     if (UserDefinedFunctions.fncRegexFind("Folio", reg.ROUTINE_DEFINITION.Equals(null) ? " " : reg.ROUTINE_DEFINITION))
                     {
-                        Console.WriteLine(reg.ROUTINE_NAME);
+                        Console.WriteLine("NAME >> " + reg.ROUTINE_NAME + " <<");
                         Console.WriteLine(reg.ROUTINE_DEFINITION);
                         Console.WriteLine(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
-                        Console.WriteLine(formatter.Format(reg.ROUTINE_DEFINITION, "LeadingCommas=False;LeadingJoins=True;RemoveComments=False"));
+                        Console.WriteLine(reg.ROUTINE_DEFINITION);
                         Console.WriteLine("////////////////////////////==================================");
                     }
                 }
@@ -35,8 +35,7 @@ namespace BucaReegexEnStoreProcedures
             Console.ReadKey();
         }
     }
-    // <TK11877:DESA21>TODO: blabkabkb  wñifgdfuqkjwa
-    // <TK12980:DESA06>COMMENT: es necesario cambiar
+
     public  class INFORMATION_SCHEMA_Result
     {
         public string ROUTINE_NAME { get; set; }
